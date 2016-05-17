035ce9f03fce0b46aebcedd95c051d13a4b8085d
AraLawrence <ara.e.lawrence@gmail.com>
2016-05-17 08:40:18 -0700

    trying material ui

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import Gif from './components/gif.js';
 
@@ -31,7 +34,9 @@ const App = React.createClass({
       <div>
         <div className="col s6 center">
           <Gif gif={this.state.gifUrl} quote={this.state.quoteText}/>
-          <div className="btn" onClick={this.handleClick}>Experience the Science</div>
+          <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <RaisedButton onClick={this.handleClick} backgroundColor="#a4c639" label="Experience the Science" />
+          </MuiThemeProvider>
         </div>
         <div style={{textAlign: 'center', marginTop: '16px'}}>
           <div className="fb-like" data-href="https://fakedomainfornow.com" data-width="290" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
